f0e5b40eab28c8d56ba0eb58a9180fa386088553
kungfuflex <judoflexchop@gmail.com>
2023-05-15 11:14:14 -0500

    first

---
+++ bin/ipfs-pinner.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const infura = new (require('ipfs-deploy/src/pinners/infura'))({ projectId: process.env.INFURA_PROJECT_ID, projectSecret: process.env.INFURA_PROJECT_SECRET });
+const yargs = require('yargs');
+
+(async () => {
+  const [ filename ] = yargs.argv._;
+  if (!filename) throw Error('must supply filename');
+  const { cid } = await infura.ipfs.add({
+    content: fs.readFileSync(filename)
+  }, { pin: true });
+  console.log(cid);
+})().catch((err) => console.error(err));
